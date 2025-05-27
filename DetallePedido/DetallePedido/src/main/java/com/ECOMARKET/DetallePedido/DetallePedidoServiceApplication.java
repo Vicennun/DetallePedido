c3322e24f2f8d7e687ab68c2e7779f4b7a04c66e
Vicennun <vicente.nunez7963@gmail.com>
2025-05-26 20:51:25 -0400

    DetallePedido conectado y funcionando falta pulir

--- DetallePedido/DetallePedido/src/main/java/com/ECOMARKET/DetallePedido/DetallePedidoServiceApplication.java
+++ DetallePedido/DetallePedido/src/main/java/com/ECOMARKET/DetallePedido/DetallePedidoServiceApplication.java
@@ -4,10 +4,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class DetallePedidoApplication {
-
-	public static void main(String[] args) {
-		SpringApplication.run(DetallePedidoApplication.class, args);
-	}
-
+public class DetallePedidoServiceApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(DetallePedidoServiceApplication.class, args);
+    }
 }
